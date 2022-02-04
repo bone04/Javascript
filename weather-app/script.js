9fb9476a9aaa595e2b0d53bec51d17f1ddd844e5
Bone4Coder <88817100+bone04@users.noreply.github.com>
2022-02-04 14:00:21 +0800

    Update script.js

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,7 +1,13 @@
-const APIURL = 'https://www.metaweather.com/api/';
+//const APIURL = 
+// 'https://www.metaweather.com/api/';
+
+const APIURL = (location) => 
+`https://cors-anywhere.hreokuapp.com/https://api/darksky.net/forecast/c1c79c3374cb0e0b5e2439d84fd12f5/${location}?exclude=minutely,hourly,daily`;
 
 async function getWeatherByLocation(location) {
-  const resp = await fetch(APIURL+'/location/search/?query='+location);
+  const resp = await fetch(APIURL+'/location/search/?query='+location, {
+    origin: "cors"
+  });
   const respData = await resp.json();
   
   console.log(respData)
