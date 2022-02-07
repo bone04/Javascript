dd04cdf9906dacffc103594c987f67b80764bd88
Bone4Coder <88817100+bone04@users.noreply.github.com>
2022-02-07 11:37:02 +0800

    Create scripts.js

---
+++ weather-app2/scripts.js
@@ -0,0 +1,9 @@
+/* SEARCH */
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector("top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
+/* SUCRIBE */
+const apiKey = "4d8fb5b93d4af21d66a2948710284366";
+
+form/addEventListener("Submit"
